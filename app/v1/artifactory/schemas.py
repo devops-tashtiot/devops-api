855128dae4298cee788259b45eb82d2624547cf7
naama steiner <naamasteiner@gmail.com>
2026-08-09 12:59:57 +0000

    schemas.py edited online with Bitbucket
    changed admin user max length

--- app/v1/artifactory/schemas.py
+++ app/v1/artifactory/schemas.py
@@ -68,7 +68,7 @@ class ProjectSpec(BaseModel):
         default=None,
         description="Username to receive PROJECT_ADMIN role",
         min_length=1,
-        max_length=14,
+        max_length=20,
         pattern=r"^[a-z][a-z0-9\-]*$",
     )
 
