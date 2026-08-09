663eeb00ee189dc91b05dddfa4a31f2d9ee686c0
naama steiner <naamasteiner@gmail.com>
2026-08-09 11:59:28 +0000

    changed minimum characters in bitbucket project key

--- app/v1/bitbucket/schemas.py
+++ app/v1/bitbucket/schemas.py
@@ -8,7 +8,7 @@ class ProjectSpec(BaseModel):
     key: str = Field(
         ...,
         description="project key",
-        min_length=1,
+        min_length=2,
         max_length=10,
         pattern=r"^[A-Z][A-Z0-9_]*$"
     )
